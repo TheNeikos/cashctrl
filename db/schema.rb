3f887d6ed346b146eedc885291d449bd5f62fd91
Marcel Müller <neikos@neikos.email>
2015-10-03 17:19:36 +0200

    Add User Sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003151203) do
+ActiveRecord::Schema.define(version: 20151003153626) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "cookie_hash"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "sessions", ["user_id"], name: "index_sessions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
