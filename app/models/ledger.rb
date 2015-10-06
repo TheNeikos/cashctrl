610f52f23a1c1f1af70407839a4298827866837e
Marcel Müller <neikos@neikos.email>
2015-10-06 20:35:29 +0200

    Add transactions to ledgers

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,5 +1,7 @@
 class Ledger < ActiveRecord::Base
   belongs_to :user
+  has_many :out_transactions, foreign_key: "sender_id"
+  has_many :in_transactions, foreign_key: "receiver_id"
 
   monetize :amount_cents
 
