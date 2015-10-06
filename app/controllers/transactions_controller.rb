709f42a7461c44293b1856563c62588bc0d11a62
Marcel Müller <neikos@neikos.email>
2015-10-06 20:37:18 +0200

    Add transaction controller & routes

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,22 @@
+class TransactionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def delete
+  end
+
+  def show
+  end
+end
