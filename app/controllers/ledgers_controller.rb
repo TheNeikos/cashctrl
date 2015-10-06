e2152bc5f623268a2b33e2d4c45722f716d19487
Marcel Müller <neikos@neikos.email>
2015-10-06 19:08:34 +0200

    Add basic ledger creation

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -2,9 +2,21 @@ class LedgersController < ApplicationController
   before_filter :require_login
 
   def new
+    @ledger = current_user.ledgers.build
   end
 
   def create
+    @ledger = current_user.ledgers.build new_ledger_params
+
+    if @ledger.save
+      redirect_to @ledger
+    else
+      render :new
+    end
+  end
+
+  def show
+    @ledger = Ledger.find(params[:id])
   end
 
   def edit
@@ -17,5 +29,11 @@ class LedgersController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def new_ledger_params
+    params.require(:ledger).permit(:name, :description, :amount, :amount_currency)
   end
 end
