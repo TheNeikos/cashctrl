3943898733f9c63ae9e1fa38ebf1a6d141d44139
Marcel Müller <neikos@neikos.email>
2015-10-06 18:13:59 +0200

    Add ledgers

---
+++ app/controllers/ledgers_controller.rb
@@ -0,0 +1,19 @@
+class LedgersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :ledgers
+
   resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :delete]
