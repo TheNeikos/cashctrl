f2db59d55fe368e5f134c153c0fa635da3b406bf
Marcel Müller <neikos@neikos.email>
2015-10-09 16:39:12 +0200

    Add transaction callbacks

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,19 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :sender, class_name: "Ledger"
-  belongs_to :receiver, class_name: "Ledger"
+  belongs_to :sender, class_name: "Ledger", :inverse_of => :out_transactions
+  belongs_to :receiver, class_name: "Ledger", :inverse_of => :in_transactions
 
-  monetize :amount
+  validates :sender, presence: true
+  validates :receiver, presence: true
+
+  monetize :amount_cents
+
+  after_create do
+    self.sender.update_attribute :amount, self.sender.amount - self.amount
+    self.receiver.update_attribute :amount, self.receiver.amount + self.amount
+  end
+
+  after_destroy do
+    self.sender.update_attribute :amount, self.sender.amount + self.amount
+    self.receiver.update_attribute :amount, self.receiver.amount - self.amount
+  end
 end
